486c8570fb51903f4f0d7357b93035b45d940889
BaluNeu <mundlamuri.b@northeastern.edu>
2024-06-09 17:04:53 -0400

    initial

---
+++ src/main/java/com/DDJAVA/DatadogJava/DatadogJavaApplication.java
@@ -0,0 +1,13 @@
+package com.DDJAVA.DatadogJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatadogJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatadogJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DDJAVA/DatadogJava/DatadogJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DDJAVA.DatadogJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatadogJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
